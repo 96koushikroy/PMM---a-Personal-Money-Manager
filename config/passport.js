67a141a78c23b7ea5850857eb8475abeb215bc87
Koushik Roy <96koushikroy@gmail.com>
2017-08-15 18:41:45 +0600

    wallet added on the menu

--- config/passport.js
+++ config/passport.js
@@ -93,11 +93,9 @@ module.exports = function(passport) {
                     if (!rows.length) {
                         return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
                     }
-
                     // if the user is found but the password is wrong
                     if (!bcrypt.compareSync(password, rows[0].password))
                         return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
                     // all is well, return successful user
                     return done(null, rows[0]);
                 });
--- routes/users.js
+++ routes/users.js
@@ -27,29 +27,24 @@ function getBanks(cb) {
 }
 
 router.showAddBankAdmin = function (req, res) {
-
     getBanks(function(res){
         BankList = res;
     });
     res.render('addBanks',{pageTitle: 'Admin | Add Bank', data: BankList});
-
 };
 
 
 router.postAddBankAdmin = function (req, res) {
     var bankName = req.body.bname;
     var data = {
-        bankname    : bankName,
+        bankname    : bankName
     };
-
     var query = db.query("INSERT INTO banks set ? ",data, function(err, rows)
     {
         if (err)
             console.log("Error inserting : %s ",err );
     });
-
     res.render('addBanks',{message:'Data Added Successfully!'});
-
 };
 
 router.showMyBanks = function (req, res) {
@@ -58,26 +53,19 @@ router.showMyBanks = function (req, res) {
         if (error) throw error;
         res.render('addMyBank',{pageTitle: 'My Banks', bank: results[0],myBank:results[1],message: req.flash('message')});
     });
-    //res.render('addMyBank',{pageTitle: 'My Banks', bank: BankList});
 };
 
 
 router.addMyBanks = function (req, res) {
-
     var uid = req.user.uid;
     var amount = req.body.balance;
     var bid = req.body.bank;
-
 
     db.query('INSERT INTO mybanks values (?,?,?);',[uid,bid,amount], function (error, results, fields) {
         if (error) throw error;//res.render('addMyBank',{pageTitle: 'My Banks',message:error});
         req.flash('message', 'Your Bank was Added Successfully!');
         res.redirect('/mybanks');
     });
-
-
-
-
 };
 
 router.deleteMyBanks = function (req, res) {
@@ -87,8 +75,6 @@ router.deleteMyBanks = function (req, res) {
         if (error) throw error;//es.render('addMyBank',{pageTitle: 'My Banks',message:error});
         req.flash('message', 'Your Bank was Deleted Successfully!');
         res.redirect('/mybanks');
-
-        //else res.render('addMyBank',{pageTitle: 'My Banks', bank: results[0],myBank:results[1],message:'Your Bank Added Successfully!'});
     });
 };
 
@@ -104,11 +90,6 @@ router.processDeposit = function (req, res) {
         req.flash('message', 'The amount was Deposited Successfully!');
         res.redirect('/mybanks');
     });
-
-    /*db.query('select my.bid,my.currentbalance,b.bankname from mybanks my join banks b on b.id = my.bid where my.uid = ?',[uid], function (error, results, fields) {
-        if (error) throw error;//es.render('addMyBank',{pageTitle: 'My Banks',message:error});
-        res.render('bankDeposit',{pageTitle:'Deposit',banks:});
-    });*/
 };
 
 
@@ -126,11 +107,9 @@ router.processWithdraw = function (req, res) {
         //console.log('data' + results[0].currentbalance);
         var currentBalance = results[0].currentbalance - parseInt(amount);
         if(currentBalance >= 0){
-
             if(wallet){
                 db.query('insert into withdraw values (?,?,?,?,?,?); update mybanks set currentbalance = currentbalance - ? where uid = ? and bid = ?; update users set wallet = wallet + ? where uid = ?',[uid,bid,amount,description,1,date,parseInt(amount),uid,bid,parseInt(amount),uid],function (err, resultss) {
                     if(err) throw err;
-
                     req.flash('message','The amount was Withdrawn Successfully!');
                     res.redirect('/mybanks');
                 });
@@ -142,7 +121,6 @@ router.processWithdraw = function (req, res) {
                     res.redirect('/mybanks');
                 });
             }
-
         }
         else{
             req.flash('message', 'Insufficient amount! Withdrawal Failed.');
